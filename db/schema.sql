4c3f0a2399b957939ea19bc873696e131c9db3b2
Rj Tiglao <rjtiglao@gmail.com>
2019-09-21 16:20:29 -0700

    added schema and seed updates

--- db/schema.sql
+++ db/schema.sql
@@ -8,8 +8,8 @@ CREATE TABLE products(
     item_id INT NOT NULL AUTO_INCREMENT,
     product_name VARCHAR(30) NULL,
     department_name VARCHAR(30) NULL,
-    price DECIMAL NULL,
-    stock_quantity INT NULL,
+    price DECIMAL(8,2) DEFAULT 0.00,
+    stock_quantity INT DEFAULT 0,
     PRIMARY KEY(item_id)
 );
 
---
+++ db/seeds.sql
@@ -0,0 +1,25 @@
+INSERT INTO products(product_name,department_name,price,stock_quantity)
+VALUES ("Monitor", "Computer",546.32,40);
+
+INSERT INTO products(product_name,department_name,price,stock_quantity)
+VALUES ("Notebooks", "School Supplies", 14.99,256);
+
+INSERT INTO products(product_name,department_name,price,stock_quantity)
+VALUES ("Keyboard and Mouse Combo Pack","Computer", 34.99,92);
+
+INSERT INTO products(product_name,department_name,price,stock_quantity)
+VALUES ("6 Pack Sierra Water Bottles", "Grocery",8.99, 52);
+
+INSERT INTO products(product_name,department_name,price,stock_quantity)
+VALUES ("Trash Can","Kitchen Appliances", 145.99,23);
+
+
+-- item_id (unique id for each product)
+
+-- product_name (Name of product)
+
+-- department_name
+
+-- price (cost to customer)
+
+-- stock_quantity (how much of the product is available in stores)
