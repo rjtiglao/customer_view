6a5de5e26965e3a5efa36d5ed108beda55e4b07e
Rj Tiglao <rjtiglao@gmail.com>
2019-09-21 16:18:32 -0700

    small tweaks

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const connection = mysql.createConnection({
     host: "localhost",
     port: 3306,
     user: "root",
-    password: "Ry051987kn061485",
+    password: "",
     database: "bamazon"
 });
 
